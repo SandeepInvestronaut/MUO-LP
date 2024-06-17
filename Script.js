056bada1949fc1a764a8a198570449a1b3d92393
SandeepInvestronaut <sandeep@investronaut.com>
2024-06-17 15:55:55 +0530

    back to top button

--- Script.js
+++ Script.js
@@ -254,3 +254,25 @@ function openTabs(el) {
   
   btnTarget.classList.add("active");
 }
+
+
+
+
+/*Back-To-Top JS */
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+/*-----END------*/
